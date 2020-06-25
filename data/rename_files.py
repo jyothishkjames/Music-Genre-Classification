01871d79950c7a678b7fb6c41ee669deec49ebfb
jyothishkjames <jyothishlp@gmail.com>
2020-06-25 17:43:13 +0200

    restructuring folder structure, code refactor

---
+++ data/rename_files.py
@@ -0,0 +1,13 @@
+__author__ = "Jyothish James"
+
+import os
+
+file_names = []
+
+file_names = [filename.split('.')[0]
+              for filename in os.listdir('/home/jyothish/Downloads/jazz_label')
+              if filename.endswith('.wav')]
+
+for i in range(len(file_names)):
+    os.rename('/home/jyothish/Downloads/jazz_wav/' + file_names[i] + '.wav',
+              '/home/jyothish/Downloads/alternative_wav/alternative_' + file_names[i] + '.wav')
