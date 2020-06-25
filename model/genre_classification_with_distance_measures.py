0adeda9e77cd3f0d730732ce58ca019dcb5f7602
jyothishkjames <jyothishlp@gmail.com>
2020-06-25 17:46:21 +0200

    restructuring folder structure, code refactor

--- model/genre_classification_with_distance_measures.py
+++ model/genre_classification_with_distance_measures.py
@@ -1,43 +1,38 @@
+__author__ = "Jyothish James"
 
-__author__  = "Jyothish James"
-
-
-import scipy.io.wavfile 
+import os
+import pickle
+import scipy.io.wavfile
 import numpy as np
 from scikits.talkbox.features import mfcc
-import numpy
-import os
-import sys
-import pickle
-from nnsearch import NearestNeighborSearch
-from simsearch import SimilaritySearch
+from sklearn.neighbors import NearestNeighbors
 
 file_names = []
 
 file_names = [filename.split('.')[0]
-              for  filename in os.listdir('/home/jyothish/Downloads/Total_wav_2')
-                if filename.endswith('.wav')]
+              for filename in os.listdir('/home/jyothish/Downloads/Total_wav_2')
+              if filename.endswith('.wav')]
 
 sample_rate, X = scipy.io.wavfile.read('/home/jyothish/Downloads/pop_wav/jazz_ATMA-Jack_s_Song.wav')
 
-#MFCC calculation for test sample
+# MFCC calculation for test sample
 ceps, mspec, spec = mfcc(X[217350:226170])
 num_ceps = len(ceps)
 X = []
 X.append(np.mean(ceps[int(num_ceps / 10):int(num_ceps * 9 / 10)], axis=0))
-Vx = np.array(X) # use Vx as input values vector for neural net, k-means, etc
-   
+Vx = np.array(X)  # use Vx as input values vector for neural net, k-means, etc
+
 with open('/home/jyothish/Downloads/Features/total_features.pickle', 'rb') as handle:
     read = pickle.load(handle)
-    
+
 sorted_file_names = sorted(file_names)
 
-nnsearch = NearestNeighborSearch()
+nnsearch = NearestNeighbors()
 
 for i in range(len(read)):
     nnsearch.add_features(read[i])
-    
-print(nnsearch.featurecount())   
+
+print(nnsearch.featurecount())
 
 dist_dictionary = nnsearch.apply(query_feature=Vx[0])
 
@@ -45,7 +40,7 @@ distance_list = []
 sorted_distance_list = []
 
 for i in range(len(sorted_file_names)):
-    sorted_dist_dictionary = {dist_dictionary['distances'][sorted_file_names[i]] : sorted_file_names[i]}
+    sorted_dist_dictionary = {dist_dictionary['distances'][sorted_file_names[i]]: sorted_file_names[i]}
     distance_list.append(sorted_dist_dictionary)
 
 sorted_distance_list = sorted(distance_list)
--- model/genre_classification_with_machine_learning_algorithms.py
+++ model/genre_classification_with_machine_learning_algorithms.py
@@ -1,27 +1,17 @@
+__author__ = "Jyothish James"
 
-__author__  = "Jyothish James"
-
-
-import scipy.io.wavfile 
+import os
+import pickle
+import random
+import scipy.io.wavfile
 import numpy as np
 from scikits.talkbox.features import mfcc
-import numpy
-import os
-import sys
-import pickle
+from sklearn import svm, tree, cluster
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import svm
-from sklearn.metrics import confusion_matrix
-import random
-from sklearn.metrics import accuracy_score
-from sklearn import tree
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_val_score
-from sklearn import cluster, datasets
-
 
 
 rows = []
@@ -37,124 +27,108 @@ test_predicted = []
 file_names = []
 
 file_names = [filename.split('.')[0]
-              for  filename in os.listdir('/home/jyothish/Downloads/Total_wav_2')
-                if filename.endswith('.wav')]
-
+              for filename in os.listdir('/home/jyothish/Downloads/Total_wav_2')
+              if filename.endswith('.wav')]
 
 sample_rate, X = scipy.io.wavfile.read('/home/jyothish/Downloads/pop_wav/jazz_ATMA-Jack_s_Song.wav')
-#MFCC calculation for test sample
+
+# MFCC calculation for test sample
 ceps, mspec, spec = mfcc(X[217350:226170])
 num_ceps = len(ceps)
-X = []
-X.append(np.mean(ceps[int(num_ceps / 10):int(num_ceps * 9 / 10)], axis=0))
-Vx = np.array(X) # use Vx as input values vector for neural net, k-means, etc
+X = [np.mean(ceps[int(num_ceps / 10):int(num_ceps * 9 / 10)], axis=0)]
+Vx = np.array(X)  # use Vx as input values vector for neural net, k-means, etc
 
-#print(Vx[0])
-    
 with open('/home/jyothish/Downloads/Features/total_features.pickle', 'rb') as handle:
     read = pickle.load(handle)
-    
-#random.shuffle(read,random.random)
-    
-#print(read[0].values()[0])
 
-#print(read)
-
+random.shuffle(read, random.random)
 
 for i in range(741):
     train.append(read[i].values()[0])
     train_label.append(read[i].keys()[0])
-    
-#print(train)
-#print(train_label)
 
 
-for j in range(742,1059):
+for j in range(742, 1059):
     test.append(read[j].values()[0])
     test_label.append(read[j].keys()[0])
 
+print("Ground Truth", len(test_label))
 
-print("Ground Truth",len(test_label))
+# SUPERVISED LEARNING
+# -------------------
 
-#SUPERVISED LEARNING
-#-------------------
+# KNN
+neighbors = KNeighborsClassifier(n_neighbors=11)
+neighbors.fit(train,train_label)
 
-#KNN
-#neighbors = KNeighborsClassifier(n_neighbors=11)
-#neighbors.fit(train,train_label)
+# SVM
+clf = svm.SVC(gamma=0.001, C=100)
+clf.fit(train, train_label)
 
-#SVM
-#clf = svm.SVC(gamma=0.001, C=100)
-#clf.fit(train, train_label)
+# Decision Tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(train, train_label)
 
-#Decision Tree
-#clf = tree.DecisionTreeClassifier()
-#clf = clf.fit(train, train_label)
+# Random Forest
+clf = RandomForestClassifier(n_estimators=100)
+clf = clf.fit(train, train_label)
 
-#Random Forest
-#clf = RandomForestClassifier(n_estimators=100)
-#clf = clf.fit(train, train_label)
+# ExtraTrees
+clf = ExtraTreesClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0)
+clf = clf.fit(train, train_label)
 
-#ExtraTrees
-#clf = ExtraTreesClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0)
-#clf = clf.fit(train, train_label)
+# Adaboost
+clf = AdaBoostClassifier(n_estimators=100)
+clf = clf.fit(train, train_label)
 
-#Adaboost
-#clf = AdaBoostClassifier(n_estimators=100)
-#clf = clf.fit(train, train_label)
+# GaussianNaiveBayers
+gnb = GaussianNB()
+gnb = gnb.fit(train, train_label)
 
-#GaussianNaiveBayers
-#gnb = GaussianNB()
-#gnb = gnb.fit(train, train_label)
-
-#Cross Validation
-#scores = cross_val_score(clf, train, train_label,cv=5)
-#print("Scores",scores)
-#print("Accuracy",scores.mean())
+# Cross Validation
+scores = cross_val_score(clf, train, train_label,cv=5)
+print("Scores", scores)
+print("Accuracy", scores.mean())
 
 
-#UNSUPERVISED LEARNING
-#---------------------
+# UNSUPERVISED LEARNING
+# ---------------------
 
 k_means = cluster.KMeans(n_clusters=4)
 k_means.fit(train)
 print(k_means.labels_)
 print(train_label)
 
+for h in range(len(test)):
+    # SVM
+    test_predicted.append(clf.predict(test[h])[0])
+    # KNN
+    test_predicted.append(neighbors.predict(test[h])[0])
+    # Decision Tree
+    test_predicted.append(clf.predict(test[h])[0])
+    # Random Forest
+    test_predicted.append(clf.predict(test[h])[0])
+    # ExtraTrees
+    test_predicted.append(clf.predict(test[h])[0])
+    # GaussianNaiveBayers
+    test_predicted.append(gnb.predict(test[h])[0])
+    # Adaboost
+    test_predicted.append(clf.predict(test[h])[0])
 
-#for h in range(len(test)):
-    #SVM
-    #test_predicted.append(clf.predict(test[h])[0])
-    #KNN
-    #test_predicted.append(neighbors.predict(test[h])[0])
-    #Decision Tree
-    #test_predicted.append(clf.predict(test[h])[0])
-    #Random Forest
-    #test_predicted.append(clf.predict(test[h])[0])
-    #ExtraTrees
-    #test_predicted.append(clf.predict(test[h])[0])
-    #GaussianNaiveBayers
-    #test_predicted.append(gnb.predict(test[h])[0])
-    #Adaboost
-    #test_predicted.append(clf.predict(test[h])[0])
-    
-    
-    
-#print("Predicted",len(test_predicted))
-    
-#print("Confusion Matrix",confusion_matrix(test_label, test_predicted))
 
-ac=accuracy_score(test_predicted,test_label)
+print("Predicted", len(test_predicted))
+
+print("Confusion Matrix", confusion_matrix(test_label, test_predicted))
+
+ac = accuracy_score(test_predicted, test_label)
 
 print("Accuracy", ac)
 
-#tn, fp, fn, tp = confusion_matrix(test_label, test_predicted).ravel()
+tn, fp, fn, tp = confusion_matrix(test_label, test_predicted).ravel()
 
-#print(tn, fp, fn, tp)
+precision = (float(tp)/float(tp+fp))
+recall = (float(tp)/float(tp+fn))
+accuracy = (float(tp+tn)/float(len(test)))
 
-#precision = (float(tp)/float(tp+fp))
-#recall = (float(tp)/float(tp+fn))
-#accuracy = (float(tp+tn)/float(len(test)))
-
-
-
+print(tn, fp, fn, tp)
+print(precision, recall, accuracy)
